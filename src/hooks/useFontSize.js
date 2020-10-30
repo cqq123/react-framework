9fdc2c1b3c843940152e1a7fa9e04b76f9859810
chenqiaoqiao <1245226827@qq,com>
2020-10-30 11:26:03 +0800

    add fontsize hook

---
+++ src/hooks/useFontSize.js
@@ -0,0 +1,9 @@
+import { useContext } from 'react';
+import FontSizeContext from 'contexts/FontSize';
+
+const useFontSize = () => {
+  const fontSize = useContext(FontSizeContext);
+  return fontSize;
+};
+
+export default useFontSize;
